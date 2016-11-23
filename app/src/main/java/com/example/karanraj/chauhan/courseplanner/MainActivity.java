e713baa4d593e5aabe6bdcd1ad24a84130bd4c6f
Anand Sanmukhani <anandsanmukhani@me.com>
2016-11-22 23:52:31 -0500

    Anand added comments 11/22/2016

--- app/src/main/java/com/example/karanraj/chauhan/courseplanner/MainActivity.java
+++ app/src/main/java/com/example/karanraj/chauhan/courseplanner/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Mic Testing 1 2 3 1 2 3
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
