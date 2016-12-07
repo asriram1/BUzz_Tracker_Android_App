917787b730668c78f3fa421879b02d450879027d
Karanraj Chauhan <chauhank@bu.edu>
2016-12-07 00:23:13 -0500

    BeverageIntake class added and implemented

---
+++ app/src/main/java/com/example/karanraj/chauhan/courseplanner/BeverageIntake.java
@@ -0,0 +1,31 @@
+package com.example.karanraj.chauhan.courseplanner;
+
+/**
+ * Created by karanraj on 12/7/16.
+ */
+
+public class BeverageIntake {
+
+    private String mName;
+    private int mQuantity;
+    private String mTime;
+
+    public BeverageIntake(String name, int quantity, String time) {
+        mName = name;
+        mQuantity = quantity;
+        mTime = time;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public int getQuantity() {
+        return mQuantity;
+    }
+
+    public String getTime() {
+        return mTime;
+    }
+
+}
--- app/src/main/java/com/example/karanraj/chauhan/courseplanner/PacerActivity.java
+++ app/src/main/java/com/example/karanraj/chauhan/courseplanner/PacerActivity.java
@@ -5,10 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -16,17 +14,13 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.R.attr.onClick;
-import static android.R.attr.value;
-import static com.example.karanraj.chauhan.courseplanner.R.id.tv;
-import static com.example.karanraj.chauhan.courseplanner.R.string.weight;
-
 /**
- * Created by karanraj on 12/3/16.
+ * Created by karanraj on 12/3/16
+ * Pacer functionality of app gives the user an estimate of their BAC at regular intervals of time
+ * based on alcohol consumption history
  */
 
 // TODO: 12/6/16 notify when gender is not selected/set default gender
-// TODO: 12/6/16 make class for beverage
 
 public class PacerActivity extends AppCompatActivity {
 
@@ -44,25 +38,8 @@ public class PacerActivity extends AppCompatActivity {
     private NumberPicker mWeightTensNumberPicker;
     private NumberPicker mWeightOnesNumberPicker;
 
-
-    // Arrays containing options inside spinners that user can select
-    private String[] BEVERAGE_OPTIONS_ARRAY = {"Regular Beer (5%, 12oz)", "Light Beer (4%, 12oz)",
-            "Table Wine (12%, 5oz)", "Wine Cooler (5%, 12oz)", "Vodka (40%, 1.25oz)",
-            "Gin (40%, 1.25oz)", "Rum (40%, 1.25oz)", "Tequila (40%, 1.25oz)",
-            "Bourbon (40%, 1.25oz)", "Scotch (40%, 1.25oz)"};
-
-    private Integer[] QUANTITY_OPTIONS_ARRAY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-
-    private String[] TIME_OPTIONS_ARRAY = {"00:00", "00:30", "01:00", "01:30", "02:00", "02:30",
-            "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00",
-            "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
-            "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00",
-            "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
-            "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"};
-
-    private List<String> beverageNamesSelected = new ArrayList<>(10);
-    private List<Integer> quantitiesSelected = new ArrayList<>(10);
-    private List<String> timesSelected = new ArrayList<>(10);
+    // ArrayList that will contain all user inputs about beverages consumption, i.e., name, quantity and time
+    private List<BeverageIntake> beverageIntakes = new ArrayList<>(5);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -92,6 +69,21 @@ public class PacerActivity extends AppCompatActivity {
         mQuantitySpinner = (Spinner) findViewById(R.id.quantity_spinner);
         mTimeSpinner = (Spinner) findViewById(R.id.time_spinner);
 
+        // Arrays containing options inside spinners that user can select
+        String[] BEVERAGE_OPTIONS_ARRAY = {"Regular Beer (5%, 12oz)", "Light Beer (4%, 12oz)",
+                "Table Wine (12%, 5oz)", "Wine Cooler (5%, 12oz)", "Vodka (40%, 1.25oz)",
+                "Gin (40%, 1.25oz)", "Rum (40%, 1.25oz)", "Tequila (40%, 1.25oz)",
+                "Bourbon (40%, 1.25oz)", "Scotch (40%, 1.25oz)"};
+
+        Integer[] QUANTITY_OPTIONS_ARRAY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+
+        String[] TIME_OPTIONS_ARRAY = {"00:00", "00:30", "01:00", "01:30", "02:00", "02:30",
+                "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00", "06:30", "07:00",
+                "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
+                "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00",
+                "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30",
+                "21:00", "21:30", "22:00", "22:30", "23:00", "23:30"};
+
         // Creating and setting adapters for spinners
         ArrayAdapter<String> beverageOptionsSpinnerAdapter = new ArrayAdapter<String>(PacerActivity.this,
                 R.layout.support_simple_spinner_dropdown_item, BEVERAGE_OPTIONS_ARRAY);
@@ -109,11 +101,12 @@ public class PacerActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                beverageNamesSelected.add(mBeverageOptionsSpinner.getSelectedItem().toString());
-                quantitiesSelected.add((Integer) mQuantitySpinner.getSelectedItem());
-                timesSelected.add(mTimeSpinner.getSelectedItem().toString());
+                beverageIntakes.add(new BeverageIntake(mBeverageOptionsSpinner.getSelectedItem().toString(),
+                        (int) mQuantitySpinner.getSelectedItem(), mTimeSpinner.getSelectedItem().toString() ));
                 mUserWeight = +100*mWeightHundredsNumberPicker.getValue()+10*mWeightTensNumberPicker.getValue()+mWeightOnesNumberPicker.getValue();
                 Log.d(TAG, "onClick: weight is "+mUserWeight);
+                BeverageIntake first = beverageIntakes.get(0);
+                Log.d(TAG, "onClick: beverage is "+first.getName()+" "+first.getQuantity()+" "+first.getTime());
             }
         });
     }
